173022af2dd6d60ebb08785f44c95882a17657a1
kgiles <info@kgiles.com>
2020-04-13 15:43:51 -0700

    Favourites page styling - no favourites
    KG commit - Added styles to favourites page for when no movies have been favourited yet.

--- src/components/Favourites.js
+++ src/components/Favourites.js
@@ -3,9 +3,11 @@ import React from 'react';
 class Favourites extends React.Component {
     render(){
         return(
-            <main class="main-favourites">
-                <section class="section-favourites">
-                    <h1 class="favourites-h1">This is a Favourites component.</h1>
+            <main className="main-favourites">
+                <section className="section-favourites">
+                    <div className="favourites-page">
+                        <p className="favourites-p">Create a list of your favourite movies by clicking the 'add to favourites' button on each individual movie page.</p>
+                    </div>
                 </section>  
             </main>
         )
