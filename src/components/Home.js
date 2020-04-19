2e5551e588435c4e62bcc8d454ffe09ede68871e
Faris <fasic@my.bcit.ca>
2020-04-18 21:24:48 -0700

    added popular movie list

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,7 +1,22 @@
 import React from 'react';
+import {API_KEY, BASE_URL} from '../globals/variables';
 
-class Home extends React.Component {
-    render(){
+const homePage = () => {
+    const [movieData, setMovieData] = useState(null);
+
+    useEffect(() => {
+
+        const fetchMovieInfo = async () => {
+            const res = await fetch (`${BASE_URL}${movie_id}?api_key=${API_KEY}&language=en-US`);
+            const movieData = await res.json();
+            setMovieData(movieData);
+            console.log(movieData);
+        }
+        fetchMovieInfo();
+
+        }, [movie_id]);
+
+        
         return(
             <main className="main-home">
                 <section className="section-home">
@@ -9,7 +24,7 @@ class Home extends React.Component {
                 </section>  
             </main>
         )
-    }
+    
 }
 
 export default Home;
