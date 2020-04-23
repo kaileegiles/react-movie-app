2ed73f122ed4daf8665c570804f75b49f6d1c2ea
Erik C <52816453+Lupeys@users.noreply.github.com>
2020-04-22 20:25:43 -0700

    Update About.js

--- src/components/About.js
+++ src/components/About.js
@@ -5,8 +5,21 @@ class About extends React.Component {
         return(
             <main class="main-about">
                 <section class="section-about">
-                    <h1 class="about-h1">This is an About component.</h1>
+                    <h1 class="about-h1">Welcome to Cinedex!</h1>
                 </section>  
+                <section>
+                    <h2>About the project</h2>
+                        <p>
+                          Cinedex is the premier movie database listing movies that can easily be found via their popularity, raiting or release date! B  
+                        </p>
+                    
+                </section>
+                <section>
+                    <h2>Meet The Team</h2>
+                        <p>
+                        
+                        </p>
+                </section>
             </main>
         )
     }
