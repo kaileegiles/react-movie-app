d49d405792e5167fea157687b14b9560baf1442d
Faris <fasic@my.bcit.ca>
2020-04-22 21:29:02 -0700

    main page update

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,62 +1,35 @@
 import React, { useEffect, useState } from 'react';
 import {API_KEY, POPULAR_URL,} from '../globals/variables';
-//import {popularMovie} from '../utilities/PopularMovie';
-//import makeDate from '../utilities/dateMaker';
-//import IndividualMovie from '../components/IndividualMovie';
+import {MovieResultsTest} from './MovieResults';
 
-const Home = (props) => {
+const Home = () => {
 
-    // App states
-    // const [popularMovieData, setPopularMovieData] = useState([]);
-    const [popularMovieTitle, setPopularMovieTitle] = useState([]);
-    const [popularMovieOverview, setPopularMovieOverview] = useState([]);
-    const [popularMovieRating, setPopularMovieRating] = useState([]);
-    const [popularMovieRelease, setPopularMovieRelease] = useState([]);
+  // App states
+    const [popularMovieData, setPopularMovieData] = useState(null);
 
         useEffect(() => {
-
+            
             const fetchPopularMovieInfo = async () => {
 
-                for(let i = 0; i < 20; i++){
                     const res = await fetch (`${POPULAR_URL}?api_key=${API_KEY}&language=en-US&page=1`);
-                    const popularMovieData = await res.json();
-                    // setPopularMovieData(popularMovieData.results[i]);
-                    setPopularMovieTitle(popularMovieData.results[i].title);
-                    setPopularMovieOverview(popularMovieData.results[i].overview);
-                    setPopularMovieRating(popularMovieData.results[i].vote_average * 10);
-                    setPopularMovieRelease(popularMovieData.results[i].release_date);
-                }
+                    let data = await res.json();
+                    
+                    setPopularMovieData(data.results);
+                     
             }
             fetchPopularMovieInfo();
     
             }, []);
 
-            // Faris - here are console logs if you want to see how it's working
-            // console.log(popularMovieTitle);
-            // console.log(popularMovieOverview);
-            // console.log(popularMovieRating);
-            // console.log(popularMovieRelease);
+            console.log(popularMovieData);
 
-        return (
-            <main className="main-home">
-                <section className="section-home">
-                    <div className="movie-grid">
-                        <div className="individual-movie">
-                            <h1>{popularMovieTitle}</h1>
-                        
-                        {/* <img src={`https://image.tmdb.org/t/p/w185${poster}`} alt={movieTitle2}></img> */}
-                        
-                        <p>{popularMovieOverview}</p>
-                        <h3>Rating</h3>
-                        <p>{popularMovieRating}%</p>
-                        <h3>Release date</h3>
-                        <p>{popularMovieRelease}</p>
-                        </div>
-                    </div>
-                </section>  
-            </main>
-        );
-    
-}
+            return (
+                <main className="main-home">
+                    <section className="section-home">
+                        {popularMovieData && <MovieResultsTest results={popularMovieData} />}
+                    </section>
+                </main>
+            );
+        }
 
 export default Home;
---
+++ src/components/MovieResults.js
@@ -0,0 +1,22 @@
+import React from 'react';
+
+const movieResults = (arr) => {
+    return arr && arr.map((result, i) =>{
+        return (
+            <div key={i} className={`movie-data-0${i+1}`}>
+                <h1>{result.title}</h1>
+                {<img src={`https://image.tmdb.org/t/p/w185${result.poster_path}`} alt={result.title}></img> }
+                <p>{result.overview}</p>
+                <h3>Rating</h3>
+                <p>{result.vote_average *10}%</p>
+                <h3>Release date</h3>
+                <p>{result.release_date}</p>
+            </div>
+        );
+    } 
+)} 
+export const MovieResultsTest = (props) => (
+    <div className="movie-grid">
+        {movieResults(props.results)}
+    </div>
+);
