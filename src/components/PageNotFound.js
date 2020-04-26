00133f691847a30a0bdc852847080a831889f9d2
kgiles <info@kgiles.com>
2020-04-26 01:37:22 -0700

    Error page styling done
    KG commit - pagenotfound styling complete.

--- src/components/PageNotFound.js
+++ src/components/PageNotFound.js
@@ -1,11 +1,16 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 class PageNotFound extends React.Component {
     render(){
         return(
             <main className="main-404">
                 <section className="section-404">
-                    <h1 className="pagenotfound-h1">This is a 404 component.</h1>
+                    <div className="pagenotfound-content">
+                        <h1 className="pagenotfound-h1">404 | Page Not Found</h1>
+                        <p className="pagenotfound-p">Sorry, looks like this page doesn't exist.</p>
+                        <p className="pagenotfound-link">Return to the <Link className="error-link" to={`/`}>home page</Link>.</p>
+                    </div>
                 </section>  
         </main>
         )
