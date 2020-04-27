d86d0292eaca4cbe137abff58fb158029a0afe19
Faris <fasic@my.bcit.ca>
2020-04-26 23:54:06 -0700

    button added to fav page

--- src/components/FavMovies.js
+++ src/components/FavMovies.js
@@ -15,14 +15,21 @@ const favMovies = (arr) => {
             const date = new Date(result.release_date);
             return `${months[date.getMonth()]} ${date.getDate()}, ${date.getFullYear()}`;
         }
+        
 
         return (
             <div key={i} className={`movie-data-0${i+1}`}>
                 <div className="movie-container">
+                    <div className="rem-fav">
+                        <button className="remove-fav" onClick={`movie-data-0${i-1}`}>Remove</button>
+                    </div>
                     {<img src={`https://image.tmdb.org/t/p/w185${result.poster_path}`} alt={result.title}></img> }
                     <h2><Link className="title-link" to={`/individual-movie/${result.id}`}>{result.title}</Link></h2>
                     {text.length > MAX_LENGTH ? <p className="fav-overview">{`${text.substring(0, MAX_LENGTH)}...`}</p> : <p>{text}</p>}
                     <div className="grid-content">
+                    
+                    
+                        
                         <div className="rating">
                             <h3 className="fav-h3">Rating</h3>
                             <p className="fav-p">{result.vote_average *10}%</p>
